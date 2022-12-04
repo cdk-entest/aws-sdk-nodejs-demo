09712e8715062f0b9e29ad8587c7ca86f199dea7
entest <hai@entest.io>
2022-12-04 13:42:13 +0000

    update upload s3 js

--- upload-s3-web/upload.js
+++ upload-s3-web/upload.js
@@ -6,9 +6,20 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
+import * as path from 'path'
+import * as fs from 'fs'
+import { config } from "./config.js";
+
 
 // s3 client
-const client = new S3Client({ region: "ap-southeast-1" });
+const client = new S3Client({
+  region: "ap-southeast-1",
+  // auto loaded from environment, role
+  // credentials: {
+  //   secretAccessKey: config.secretAccessKey,
+  //   accessKeyId: config.accessKeyId
+  // }
+});
 
 // list objects in a bucket
 const listObjects = async (bucketName) => {
@@ -25,14 +36,14 @@ const listObjects = async (bucketName) => {
 };
 
 // upload an object to a bucket
-const uploadObject = async (bucketName) => {
+const uploadObject = async (bucketName, keyName) => {
   console.log(bucketName);
 
   try {
     const res = await client.send(
       new PutObjectCommand({
         Bucket: bucketName,
-        Key: "test-upload-object",
+        Key: keyName,
         Body: "Testing upload object 11223344",
       })
     );
@@ -42,6 +53,25 @@ const uploadObject = async (bucketName) => {
   }
 };
 
+const uploadFile = async (bucketName, keyName, fileName) => {
+  try {
+    const res = await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName, 
+        Key: keyName, 
+        // Body: fs.readFileSync("./data_nodejs.txt")
+        Body: fs.createReadStream("./data_nodejs.txt")
+      })
+    )
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
 //
-listObjects("haimtran-workspace");
-// uploadObject("bio-register-customer");
+// listObjects("haimtran-workspace");
+// uploadObject(config.bucketName, `test/upload_from_nodejs_${String(Date.now())}.txt`);
+uploadFile(config.bucketName, `test/${String(Date.now())}.txt`, "./data_nodejs.txt")
+
