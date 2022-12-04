559c1bc9055059de8ba01563d592eab5e643db47
entest <hai@entest.io>
2022-12-04 15:45:55 +0000

    upload with progress

--- upload-s3-web/upload.js
+++ upload-s3-web/upload.js
@@ -6,10 +6,10 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
-import * as path from 'path'
-import * as fs from 'fs'
+import { Upload } from "@aws-sdk/lib-storage";
+import * as path from "path";
+import * as fs from "fs";
 import { config } from "./config.js";
-
 
 // s3 client
 const client = new S3Client({
@@ -57,21 +57,56 @@ const uploadFile = async (bucketName, keyName, fileName) => {
   try {
     const res = await client.send(
       new PutObjectCommand({
-        Bucket: bucketName, 
-        Key: keyName, 
+        Bucket: bucketName,
+        Key: keyName,
         // Body: fs.readFileSync("./data_nodejs.txt")
-        Body: fs.createReadStream("./data_nodejs.txt")
+        Body: fs.createReadStream("./data_nodejs.txt"),
       })
-    )
-    console.log(res)
+    );
+    console.log(res);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
+// upload and check progerss
+const uploadToS3Progress = async (bucketName, keyName, fileName) => {
+  // s3 client
+  const s3Client = new S3Client({
+    region: config.REGION,
+
+    // credentials: fromCognitoIdentityPool({
+    //   clientConfig: { region: config.REGION },
+    //   identityPoolId: config.IDENTITY_POOL_ID,
+    //   logins: {
+    //     [config.COGNITO_POOL_ID]: idToken,
+    //   },
+    // }),
+  });
+
+  const upload = new Upload({
+    client: s3Client,
+    params: {
+      Bucket: bucketName,
+      Key: keyName,
+      Body: fs.createReadStream(fileName),
+    },
+  });
+  upload.on("httpUploadProgress", (progress) => {
+    let percent = progress.loaded / progress.total * 100.0
+    console.log(`upload ${progress.total}/${progress.loaded} bytes = ${percent.toFixed(2)}%`)
+  });
+
+  const startTime = Date.now()
+  await upload.done();
+  const stopTime = Date.now()
+  console.log(`duration = ${(stopTime - startTime)/1000.0} seconds`)
+
+
+};
 
 //
 // listObjects("haimtran-workspace");
 // uploadObject(config.bucketName, `test/upload_from_nodejs_${String(Date.now())}.txt`);
-uploadFile(config.bucketName, `test/${String(Date.now())}.txt`, "./data_nodejs.txt")
-
+// uploadFile(config.bucketName, `test/${String(Date.now())}.txt`, "./data_nodejs.txt")
+uploadToS3Progress(config.bucketName, `test/setup.mov`, "./setup.mov");
